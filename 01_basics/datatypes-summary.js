59e69239cd7eaf44f45ef7ee3f1733cb11514998
Sameer Gupta <156401862+synergisticgreed@users.noreply.github.com>
2025-01-18 13:01:00 +0000

    Memory in javaScript

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -34,4 +34,38 @@ console.log(typeof(myFun));
 
 
 
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+/*
+Memory :
+Stack
+Heap
+
+
+
+
+
+
+*/
+// let myYoutubeName="Nitian Sameer"
+// let anotherName=myYoutubeName
+// anotherName="Hello"
+// console.log(anotherName)
+// console.log(myYoutubeName)
+
+
+
+let user1={
+    email : "hello@gmail.com",
+    upi : "42367u3w"
+}
+
+let user2=user1
+user2.email="hello@gmail.com"
+console.log(user1)
+console.log(user2)
+
+
+
+
 
