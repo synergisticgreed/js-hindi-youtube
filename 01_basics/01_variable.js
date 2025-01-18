1ce05eb0dc57d53cd733b4b296e50a839f5a70f4
Sameer Gupta <156401862+synergisticgreed@users.noreply.github.com>
2025-01-18 10:15:55 +0000

    Variables and console table method

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId=67524526
+let accountName="Sameer"
+var password
+city="Lucknow"
+let accountState
+
+
+/*Prefer not to use var in javascript because
+of issue in block scope and functional scope */
+
+
+
+console.table([accountId,accountName,password,city,accountState])
