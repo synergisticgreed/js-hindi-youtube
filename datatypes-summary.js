7a7ab734832d4275ba2553205e9fc2b7412e3d4e
Sameer Gupta <156401862+synergisticgreed@users.noreply.github.com>
2025-01-18 12:43:48 +0000

    datatype Summary

---
+++ datatypes-summary.js
@@ -0,0 +1,37 @@
+//Primitive 
+
+
+//7 types
+// : String, Number, Boolean, null, undefined , symbol, BigInt 
+
+
+// const id = Symbol('123')
+// const anotherId= Symbol('123')
+// console.log(id===anotherId);       //false;
+// console.log(anotherId);
+
+//Refernce type / Non Primitive
+
+//Array, Objects, Functions
+
+
+const heroes=["shaktiman","naagraj", "doga"]
+
+let myObj={
+    name:"rohit",
+    class : "hello",
+}
+
+const myFun=function(){
+    console.log("Hello World");
+    
+}
+console.log(myFun());
+console.log(typeof(myFun));
+
+
+
+
+
+
+
