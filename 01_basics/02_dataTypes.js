f548a414c8928c5e63af4dbbc146bf4fa06fff89
Sameer Gupta <156401862+synergisticgreed@users.noreply.github.com>
2025-01-18 10:41:30 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all codes in newer version of js
+
+//alert(3+3)  ;     we are using nodejs , not browser
+
+
+console.log(3+
+    3        //Code reusability should be high
+)
+
+console.log("Sameer")
+/*    PRIMITIVE    */
+//number=> 2 to power 53
+//bigint
+//string => ""
+//boolean=> true/false
+//null => standalone value
+//undefined =>
+//symbol =>uniquenes of react components 
+
+
+
+//object
+
+
+
+console.log(typeof(undefined));    //undefined
+
+console.log(typeof(null));      // object
