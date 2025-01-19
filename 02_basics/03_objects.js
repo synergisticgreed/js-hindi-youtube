5f524f7f61c335167289774d4005f6fe091f69cf
Sameer Gupta <156401862+synergisticgreed@users.noreply.github.com>
2025-01-19 19:10:05 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,20 @@
+//singleton
+
+
+//Object.create      //constructor method
+//objects literals
+const Jsuser={
+    name: "Rohit",
+    "fullName" : "Rohit Sharma",
+    age: 18,
+    location: "Prayagraj",
+    email: "hello@gmail.com",
+    isloggedin: false,
+    lastLoginDays: ["Monday","Saturday"]
+}
+
+console.log(Jsuser.email);
+console.log(Jsuser["email"]);
+console.log(Jsuser.fullName);
+
+
---
+++ 02_basics/04_objects.js
@@ -0,0 +1,24 @@
+const tinderUser=new Object() 
+
+tinderUser.id="123abc"
+tinderUser.name="Sam"
+tinderUser.isLoggedIn=false
+//console.log(tinderUser);
+const regularUser = {
+    email: "Sam@gmail.com",
+    fullName:{
+        userfullName:{
+            firstname: "Sam",
+            lastname: "hello"
+        }
+    }
+}
+//console.log(regularUser);
+const obj1 ={1:"a",2:"b"}
+const obj2 ={3:"a",4:"b"}
+const obj3={...obj1,...obj2}
+console.log(obj3);
+
+
+//object assign
+
