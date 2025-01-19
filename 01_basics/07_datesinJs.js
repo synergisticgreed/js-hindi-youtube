c5a462fb4c15342685e85ab14d257232fc8b34f3
Sameer Gupta <156401862+synergisticgreed@users.noreply.github.com>
2025-01-19 12:35:34 +0000

    date and time

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,12 @@
+//Dates
+
+let myDate=new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+
+let myCreatedDate=new Date(2025,0,25)
+console.log(myCreatedDate.toDateString());
