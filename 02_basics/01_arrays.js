21e5ed13b57a87f8532ed6fa6bbd2e8fca514058
Sameer Gupta <156401862+synergisticgreed@users.noreply.github.com>
2025-01-19 18:22:59 +0000

    arrays part1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,22 @@
+//arrays
+
+const myarr=[1,2,3,4,5]
+console.log(myarr);
+
+// myarr.push(6)
+// console.log(myarr);
+// myarr.pop()
+// console.log(myarr);
+
+// myarr.unshift(0)
+// console.log(myarr);
+// myarr.shift()
+// console.log(myarr);
+// myarr.shift()
+// console.log(myarr);
+
+
+//slice, splice
+console.log("A",myarr);
+
+
