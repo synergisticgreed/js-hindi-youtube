c2221268f4f3004b167ac83074118d7239e733bf
Sameer Gupta <156401862+synergisticgreed@users.noreply.github.com>
2025-01-19 12:18:11 +0000

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,31 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(300)
+//console.log(balance.toFixed(0));
+
+
+
+//Precision
+
+
+//******************************************************Maths****************************** */
+//Default Library of Js maths
+
+
+// console.log(Math);
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(5.5));
+// ceil
+//flooor
+//min
+//max
+
+
+//console.log(Math.floor(Math.random()*10000+1))
+
+const min=1000
+const max=9999
+console.log(Math.floor((Math.random()*(max-min+1))+min));
+
