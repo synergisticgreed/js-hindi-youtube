f06228eb50d8317670b2b0629304b23739074bb7
Sameer Gupta <156401862+synergisticgreed@users.noreply.github.com>
2025-01-19 18:38:21 +0000

    arrays part2

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,19 @@
+const marvel_heroes=["thor","Ironman","spiderman"]
+const dc_heroes=["superman","flash","batman"]
+//marvel_heroes.push(dc_heroes)
+// console.log(marvel_heroes);
+// const arr=marvel_heroes.concat(dc_heroes)
+// console.log(arr);
+
+const all_heroes=[...marvel_heroes,...dc_heroes]       //spread operator
+//console.log(all_heroes);
+
+
+
+//flat function
+
+
+
+
+
+
