e7c08ed3a530c3e91023a6ba59efebfc63688d5e
Sameer Gupta <156401862+synergisticgreed@users.noreply.github.com>
2025-01-20 19:52:30 +0000

    iife discussion

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,18 @@
+//Immediately invoked function expression    (IIFE)
+// function chai(){
+//     console.log(`DB CONNECTED`);
+    
+// }
+// chai()
+
+// (function chai(){                         //normal iife
+//     console.log(`DB CONNECTED`);
+    
+// })()
+
+(()=>{
+    console.log(`DB CONNECTED`);
+    
+})()
+
+
