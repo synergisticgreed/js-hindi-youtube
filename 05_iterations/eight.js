b5f5bf31db4df133db8aab2474102a4f9bd5adab
Sameer Gupta <156401862+synergisticgreed@users.noreply.github.com>
2025-01-22 19:03:55 +0000

    filter, map, reduce

---
+++ 05_iterations/eight.js
@@ -0,0 +1,16 @@
+//reduce
+
+const nums=[1,2,3]
+const myTotal=nums.reduce((acc,currval)=>{
+   // console.log(`acc: ${acc} and currval: ${currval}`);
+    
+    return acc+currval
+},100)
+//console.log(myTotal);
+
+// const shoppingCart=[
+//     {
+//         itemName:
+
+//     }
+// ]
---
+++ 05_iterations/six.js
@@ -0,0 +1,17 @@
+const coding =["js","ruby","java","python","cpp"]
+
+// const values=coding.forEach((val)=>{               // not return anything
+//     console.log(val);
+//     return val;
+    
+// })
+//console.log(values);
+
+
+
+/////What about conditional check in array------------------------------->
+//filter
+const myNums=[1,2,3,4,5,6,7,8,9,10]
+const newNums=myNums.filter((num)=>{ return num>4})
+
+console.log(newNums);
